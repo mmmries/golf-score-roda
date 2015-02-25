fc4356c9dc383a69e98dd69c2e4c0dd836328a97
Jeremy Evans <code@jeremyevans.net>
2015-02-24 19:58:58 -0800

    Fix specs so that they work if you aren't in -0600 timezone

--- spec/models/record_book_spec.rb
+++ spec/models/record_book_spec.rb
@@ -8,7 +8,7 @@ describe RecordBook do
     expect(ron_jon_gold[:holes]).to eq [2,4,2,3,1,3,1,2,2]
     expect(ron_jon_gold[:place]).to eq 1
     expect(ron_jon_gold[:player_id]).to eq 1
-    expect(ron_jon_gold[:played_at]).to eq ::Time.parse("2012-08-16 20:59:00 -0600")
+    expect(ron_jon_gold[:played_at]).to eq ::Time.parse("2012-08-16 20:59:00")
     expect(ron_jon_gold[:course_id]).to eq 1
   end
 
